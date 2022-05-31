cc61da93fd7648050262ffa057a9c095d465c494
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-05-31 17:49:03 -0400

    add header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans app__header-p" style={{ margin: "2rem 0" }}>
+        Experience the burst of flavors of Nepali cuisine with an array of dishes from around the diverse Himalayan nation nestled
+        between mountains and plains, forests and rivers.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
     Header
   </div>
 );
