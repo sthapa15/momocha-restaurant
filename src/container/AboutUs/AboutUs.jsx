cc62f8309a0d5f76c8742fd6ef0c7672fab82e87
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-06-01 12:20:28 -0400

    complete and style about us section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
 
-import './AboutUs.css';
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.M} alt="m letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          We are Momocha, critically acclaimed cuisine from the foothills of the Himalayas. Established in 2021, we have charmed
+          diners with flavors, hospitality, and fun.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife">
+        <img src={images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          As immigrants from Nepal, we have taken our home cooked recipes and elevated them to fit a fine dining palate over the
+          years.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavor" />
-      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <h1 className="app__header-h1">The Key to Great Dining</h1>
       <p className="p__opensans app__header-p" style={{ margin: "2rem 0" }}>
         Experience the burst of flavors of Nepali cuisine with an array of dishes from around the diverse Himalayan nation nestled
         between mountains and plains, forests and rivers.
