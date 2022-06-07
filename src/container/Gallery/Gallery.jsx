0c108e433cbb3167e87c44c45adfcf460cccf9ac
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-06-07 14:23:13 -0400

    complete contact and gallery sections

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 
-import { SubHeading, MenuItem } from "../../components";
+import { SubHeading } from "../../components";
 import { images } from "../../constants";
 
 import "./Gallery.css";
@@ -27,7 +27,7 @@ const Gallery = () => {
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
         <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
-          Lorem upsumsdlkfj
+          Visit our instagram for more pictures from our restaurant and give us a follow for events and updates.
         </p>
         <button type="button" className="custom__button">
           View More
@@ -37,13 +37,13 @@ const Gallery = () => {
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
           {galleryImages.map((image, index) => (
-            <div className="app__gallery-images_card flex__center" key={`gallery-image-${index + 1}`}>
-              <img src={image} alt="gallery" />
-              <BsInstagram className="gallery__image-icon"/>
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
-        <div className="app__gallery-images_arrow">
+        <div className="app__gallery-images_arrows">
           <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll("left")} />
           <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll("right")} />
         </div>
