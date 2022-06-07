11a5466704b21145217e90959626c238390c79f5
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-06-07 17:30:41 -0400

    add footer section modify chefimage

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Newsletter.css";
 
-const Newsletter = () => {
-  return <div>Newsletter</div>;
-};
+import SubHeading from "../SubHeading/SubHeading";
+
+const Newsletter = () => (
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss the latest updates!</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">
+        Subscribe
+      </button>
+    </div>
+  </div>
+);
 
 export default Newsletter;
